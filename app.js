4886c51ed2ff20b259514a5ee28bcab4321d417d
supermazlum <mazlum.superc@gmail.com>
2023-06-13 14:25:18 +0200

    2. push

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
-// const fs = require('fs');
+const fs = require('fs');
 const app = express();
 
 let visitorCount = 0;
+const countFilePath = 'visitorCount.json';
 
-// LEVEL 1
+try {
+    const data = fs.readFileSync(countFilePath, 'utf8');
+    const countData = JSON.parse(data);
+    visitorCount = countData.count;
+    console.log('Visitor count loaded:', visitorCount);
+} catch (err) {
+    if (err.code === 'ENOENT') {
+        console.log('File not found. Starting with visitorCount:', visitorCount);
+        saveVisitorCount();
+    } else {
+        console.log('Error reading file:', err);
+    }
+}
+
+function saveVisitorCount() {
+    const countData = { count: visitorCount };
+    fs.writeFileSync(countFilePath, JSON.stringify(countData), 'utf8');
+    console.log('Visitor count saved.');
+}
+
 app.get('/visit', (req, res) => {
     visitorCount++;
     res.send('Visited successfully.');
+    saveVisitorCount();
 });
 
 app.get('/visited', (req, res) => {
@@ -17,38 +38,3 @@ app.get('/visited', (req, res) => {
 app.listen(3007, () => {
     console.log('Server started on port 3007');
 });
-
-
-// LEVEL 2
-fs.readFile('visitorCount.json', 'utf8', (err, data) => {
-    if (err) {
-        console.log('Error reading file:', err);
-    } else {
-        const countData = JSON.parse(data);
-        visitorCount = countData.count;
-        console.log('Visitor count loaded:', visitorCount);
-    }
-});
-
-app.get('/visit', (req, res) => {
-    visitorCount++;
-    res.send('Visited successfully.');
-
-    const countData = { count: visitorCount };
-    fs.writeFile('visitorCount.json', JSON.stringify(countData), 'utf8', (err) => {
-        if (err) {
-            console.log('Error writing file:', err);
-        }
-    });
-});
-
-app.get('/visited', (req, res) => {
-    res.send(`Total visitors: ${visitorCount}`);
-});
-
-app.listen(3001, () => {
-    console.log('Server started on port 3001');
-});
-
-
-
